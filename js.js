8a9ad2e7fbf3a88d28c1d73ef398bb1925e8c821
unknown <kevinlukas1803@gmail.com>
2023-03-23 22:10:51 +0700

    js.js

---
+++ js.js
@@ -0,0 +1,15 @@
+ function tglsekarang () {
+     const tanggalbaru = new Date
+    console.log(tanggalbaru); 
+ }
+
+ tglsekarang()
+
+function tglsekarang() {
+    const tanggalbaru = new Date();
+    return tanggalbaru;
+}
+  
+   const tanggal = tglsekarang();
+   console.log(tanggal);
+  
