52c8cc0d6bc54da5c9f1443bf6e481db7174bd15
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-03-23 22:10:31 +0700

    Tanggal

--- Mewah.js
+++ Mewah.js
@@ -1,3 +1,11 @@
+// Kerjakan Soal - Soal dibawah ini dalam bentuk live code:
+// Buatlah sebuah function.
+// Function tersebut akan mencetak tanggal sekarang ketika dipanggil.
+// Buatlah function berikutnya.
+// Function tersebut memberikan tanggal sekarang ketika dipanggil.
+// Cetaklah hasil dari function tersebut.
+// Buatlah pseudocode dan programnya!
+
 // writeCurrentDate()
 function writeCurrentDate() {
   const currentDate = new Date(); // return 0 GMT
