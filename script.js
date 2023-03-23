d21e4bd8a04d7ea9adc3a9094bfd3663a4402b90
Rifqiyah162001 <rifqiyahramadani62@gmail.com>
2023-03-23 22:05:27 +0700

    live code 3

---
+++ script.js
@@ -0,0 +1,15 @@
+//function cetak tanggal sekarang
+function getDate(){
+    const date = new Date()
+    console.log(date)
+}
+
+getDate();
+
+//function memberikan tanggal sekarang
+function dateReturn() {
+    const date = new Date()
+    return date
+}
+
+console.log(dateReturn());
