3f505739adfaed6984a6b620c316dc33b7f78211
Rivaldi Alwi <rivaldialwi42@gmail.com>
2023-03-23 22:01:21 +0700

    Update js live code 3

---
+++ main.js
@@ -0,0 +1,14 @@
+// function dateNow () {
+//     const newDate = new Date
+//     console.log(newDate); 
+// }
+// dateNow()
+
+// function dateNow() {
+//     const newDate = new Date();
+//     return newDate;
+//   }
+  
+//   const tanggal = dateNow();
+//   console.log(tanggal);
+  
