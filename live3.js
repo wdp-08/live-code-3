ba218747f01ca4939fb47f30ef0ba88b3f551f8b
Delin Herliana Pashs <delinherlianapasha@gmail.com>
2023-03-23 22:17:03 +0700

    live3

---
+++ live3.js
@@ -0,0 +1,9 @@
+// mencetak tanggal sekarang
+// let tanggal = new Date("2023-03-23");
+// alert(tanggal);
+// document.getElementById("hasil").innerHTML = tanggal;
+
+let tanggal = function () {
+  Date("2023-03-23");
+};
+console.log(tanggal);
