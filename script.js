92c8504d72a35439e72e4bf4d4819cf3780a6896
Dodistywn <dodisetya000@gmail.com>
2023-03-23 22:12:28 +0700

    manmbahkan

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+function printCurrenDate(){
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String( currentDate.getMonth() + 1).padStart(2, '0');
+    const day = currentDate.getFullYear();
+
+}
+
 const tanggal = new Date();
 document.getElementById("tgl").innerHTML = tanggal;
+console.log(tanggal);
 
 const tanggal2 = new Date().getDate();
 document.getElementById("tgl2").innerHTML = tanggal2;
+
+console.log(getCurrendat());
