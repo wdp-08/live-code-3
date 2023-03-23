be0b315ece4cb217e27fe204832b8de0e9e188a5
Mewah Aprilia Susyanti <111202012801@mhs.dinus.ac.id>
2023-03-23 21:56:54 +0700

    Tanggal

---
+++ Mewah.js
@@ -0,0 +1,14 @@
+function writeCurrentDate() {
+  const currentDate = new Date(); // return 0 GMT
+  console.log(currentDate);
+}
+// writeCurrentDate()
+
+//   function returnCurrentDate() {
+//     const currentDate = new Date();
+
+//     return currentDate;
+//   }
+
+//   const resultDate = returnCurrentDate();
+//   console.log(resultDate);
